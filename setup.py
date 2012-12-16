171595afb28b1da1ae7a0954dc5db46c69e7502a
Tim Swast <tswast@gmail.com>
2012-12-16 12:43:10 -0600

    Added support for semi-official wiiuse fork. Thanks to Juan Alvarez.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='wiiuse',
-    version='0.12',
+    version='0.13',
     author='Tim Swast',
     author_email='tswast@gmail.com',
 
